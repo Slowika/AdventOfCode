bd43f752d9b5f5678bd3f5a99a41f9831bba7d5c
Agnieszka Slowik <agnieszka.slowik44@gmail.com>
2015-12-21 16:35:12 +0100

    task2

--- src/Advent2.java
+++ src/Advent2.java
@@ -7,8 +7,10 @@ import java.util.*;
 public class Advent2 {
     public static void main (String[] args) {
         Scanner inScan = new Scanner(System.in);
-        int l, w, h, min, min1 = 0;
+        int l, w, h, min, min1, maxSide1, maxSide = 0;
         int total = 0;
+        int totalRibbon = 0;
+
         while(inScan.hasNext()){
             String a = inScan.next();
             String[] arr = a.split("x");
@@ -16,11 +18,21 @@ public class Advent2 {
             w = Integer.parseInt(arr[1]);
             h = Integer.parseInt(arr[2]);
 
-            min1 = Integer.min(l*w, w*h);
-            min = Integer.min(min1, h*l);
+           // min1 = Integer.min(l*w, w*h);
+            //min = Integer.min(min1, h*l);
 
-            total += 2*l*w + 2*w*h + 2*h*l + min;
-            System.out.println(total);
+            maxSide1 = Integer.max(l, w);
+            maxSide = Integer.max(maxSide1, h);
+
+           // total += 2*l*w + 2*w*h + 2*h*l + min;
+
+
+
+            totalRibbon += 2*(l + w + h - maxSide);
+
+            totalRibbon += (l*w*h);
+
+            System.out.println(totalRibbon);
 
         }
     }
